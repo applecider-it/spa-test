5518b6033e9b4d1f17a69b40ea1d55ea5726b661
owner <owner@localhost>
2026-03-17 15:45:27 +0900

    phpサーバーの整備

--- php/app/Services/Server/DispatchService.php
+++ php/app/Services/Server/DispatchService.php
@@ -4,88 +4,40 @@ namespace App\Services\Server;
 
 use App\Services\Output\LogService as Log;
 
+use App\Services\Auth\AuthService;
+
 /**
  * ルートごとの処理
  */
 class DispatchService
 {
+    use DispatchService\AuthDispatchService;
+    use DispatchService\DevelopmentDispatchService;
+
+    private AuthService $authService;
+
     function __construct(
         private string $method,
         private string $uri,
         private ?array $jsonData,
-    ) {}
+    ) {
+        $this->authService = new AuthService;
+    }
 
     public function exec(): array
     {
         if ($this->uri === '/send-test') {
-            return $this->execSendTest();
+            return $this->execDevelopmentSendTest();
         } else if ($this->uri === '/auth') {
             return $this->execAuth();
         } else if ($this->uri === '/login') {
-            return $this->execLogin();
+            return $this->execAuthLogin();
         } else if ($this->uri === '/logout') {
-            return $this->execLogout();
+            return $this->execAuthLogout();
         }
 
         return [
             'error' => 'invalid uri',
-        ];
-    }
-
-    /** 送信テスト */
-    private function execSendTest(): array
-    {
-        $data = [
-            'method' => $this->method,
-            'uri' => $this->uri,
-            'message' => 'テストメッセージ ' . $this->jsonData['message'],
-            'jsonData' => $this->jsonData,
-        ];
-
-        if (!isset($_SESSION['cnt'])) $_SESSION['cnt'] = 0;
-
-        $_SESSION['cnt']++;
-
-        return $data;
-    }
-
-    /** 認証チェック */
-    private function execAuth(): array
-    {
-        usleep(1000000 * 0.3);
-
-        $userId = $_SESSION['user_id'] ?? null;
-
-        $user = null;
-        if ($userId) {
-            $user = [
-                'name' => 'テスト',
-            ];
-        }
-        $data = [
-            'user' => $user,
-        ];
-
-        return $data;
-    }
-
-    /** ログイン */
-    private function execLogin(): array
-    {
-        $_SESSION['user_id'] = 1;
-
-        return [
-            'status' => true,
-        ];
-    }
-
-    /** ログアウト */
-    private function execLogout(): array
-    {
-        unset($_SESSION['user_id']);
-
-        return [
-            'status' => true,
         ];
     }
 }
