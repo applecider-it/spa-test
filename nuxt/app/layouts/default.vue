b05578ec82e6030a56dc265ddf3b61975124fcd3
owner <owner@localhost>
2026-04-17 14:18:36 +0900

    レイアウト

---
+++ nuxt/app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header class="p-4 bg-blue-500 text-white">
+      ヘッダー
+    </header>
+
+    <main class="p-6">
+      <slot />
+    </main>
+
+    <footer class="p-4 bg-gray-100">
+      フッター
+    </footer>
+  </div>
+</template>
---
+++ nuxt/app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'admin'
+})
+</script>
+
+<template>
+  <div>
+    <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 class="text-4xl font-bold text-blue-500">Nuxt + Tailwind</h1>
+    </div>
+  </div>
+</template>
