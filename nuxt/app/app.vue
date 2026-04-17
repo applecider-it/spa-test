ca14464687461be2ac13abc38201a268cc753439
owner <owner@localhost>
2026-04-17 13:56:49 +0900

    axios tailwind

--- nuxt/app/app.vue
+++ nuxt/app/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 class="text-4xl font-bold text-blue-500">Nuxt + Tailwind</h1>
+    </div>
   </div>
 </template>
