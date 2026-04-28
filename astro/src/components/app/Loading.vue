3ef054748d7cfd8150f41392e3bf77355dc591cf
owner <owner@localhost>
2026-04-28 12:09:02 +0900

    ローディング演出

---
+++ astro/src/components/app/Loading.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { loading } from '@/services/app/loading';
+</script>
+
+<template>
+  <div v-show="loading" class="overlay fade-in" />
+</template>
+
+<style>
+.overlay {
+  position: fixed;
+  inset: 0;
+  background: black;
+  opacity: 0.1;
+  z-index: 999;
+  pointer-events: none;
+}
+.fade-in {
+  animation: fadeIn 2s ease-in;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 0.1;
+  }
+}
+</style>
